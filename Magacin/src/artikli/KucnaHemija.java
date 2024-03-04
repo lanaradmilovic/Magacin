ccabfba469dba93b9c7f81d10e0f56549763894e
student2 <student2@50-21.prod.fon.bg.ac.rs>
2024-03-04 15:33:19 +0100

    Initial commit. Created domain objects with getters and setters.

---
+++ Magacin/src/artikli/KucnaHemija.java
@@ -0,0 +1,17 @@
+package artikli;
+
+import java.sql.Date;
+
+public class KucnaHemija {
+	private Date rokTrajanja;
+
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
+
+	public void setRokTrajanja(Date rokTrajanja) {
+		this.rokTrajanja = rokTrajanja;
+	}
+	
+
+}
